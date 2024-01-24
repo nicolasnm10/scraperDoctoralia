cadd626fda47e45c7494a69fcd6fcaac447287f6
NM10 <nicolasemilianmn@gmail.com>
2024-01-24 15:01:28 -0300

    resolving comments in PR, check if the errors returned by the promisePool have errors throw an exception

--- src/scraper.ts
+++ src/scraper.ts
@@ -4,17 +4,16 @@ import { PromisePool } from '@supercharge/promise-pool';
 
 const getListProfessionals = async (url: string) => {
     console.log(url);
-    const response = await axios.get(url);
-    if (response.status !== 200) {
-        console.error('Error al obtener la página');
-        return [];
-    }
-    const resultslinkperfil = [];
-
     try {
+        const response = await axios.get(url);
+        console.log({ status: response.status });
+        if (response.status !== 200) {
+            console.error('Error al obtener la página');
+            return [];
+        }
+        const resultslinkperfil = [];
         const html = response.data;
         const $ = cheerio.load(html);
-
         const div = $('#search-content');
         const divsConClase = div.find('div.col-lg-6.result-column.p-2');
         const resultslinknextpage = [];
@@ -22,8 +21,6 @@ const getListProfessionals = async (url: string) => {
             const contenidoHtml = $(element);
             const contenth3 = $(contenidoHtml).find('h3.h4.mb-0.flex-wrap');
             const alinkperfil = $(contenth3).find('a.text-body').attr('href');
-            console.log(alinkperfil);
-
             resultslinkperfil.push({
                 alinkperfil
             });
@@ -31,66 +28,53 @@ const getListProfessionals = async (url: string) => {
         const ulnexturls = $('ul.pagination.pagination-lg');
         ulnexturls.each((_index, ulElement) => {
             const nexturl = $(ulElement).find('li');
-            // console.log(nexturl.html(),'next');
             nexturl.each((_liIndex, liElement) => {
                 const contenidoHtml = $(liElement);
-                // console.log(contenidoHtml.html(),'liindex');
                 const alinknextpage = $(contenidoHtml)
                     .find('a.page-link')
                     .attr('href');
-                // console.log(alinknextpage,'aaa')
                 resultslinknextpage.push({
                     alinknextpage
                 });
             });
         });
+        return resultslinkperfil;
     } catch (e) {
         console.error(`Error al obtener la página error: ${e}`);
         return [];
     }
-
-    return resultslinkperfil;
 };
 
 const processPage = async () => {
     let results = [];
     let page = 1;
-    const numbersPage = 10;
+    const numbersPage = 20;
     while (true) {
         try {
             const listPages = Array.from(
                 { length: numbersPage },
                 (_, index) => page + index
             );
-            console.log({ listPages });
             const { results: resultList, errors } =
                 await PromisePool.withConcurrency(numbersPage)
                     .for(listPages)
                     .process(async (page) => {
                         const url = `https://www.doctoralia.cl/kinesiologo/${page}`;
-                        console.log(`Get List Page ${page}`);
                         const urls = await getListProfessionals(url);
-                        console.log({ urls });
                         return urls;
                     });
-            console.log('poolResults:', resultList);
-            console.log({ errors });
-            results = results.concat(resultList.flat());
+            if (errors.length) {
+                console.log({ errors });
+                throw new Error('ERROR_FAILED_SCRAPER');
+            }
+            const flatresult = resultList.flat();
+            if (!flatresult.length) {
+                break;
+            }
+            results = results.concat(flatresult);
             page = page + numbersPage;
-            break;
-            // return list;
         } catch (error) {
             console.error('Otro error:', error.message);
-            // if (error.response && error.response.status === 404) {
-            //     console.error(
-            //         `Error 404: La página ${url} no fue encontrada. Finalizando el proceso.`
-            //     );
-            //     // return null;
-            //     break;
-            // } else {
-            //     console.error('Otro error:', error.message);
-            //     return null;
-            // }
         }
     }
     return results;
@@ -98,8 +82,6 @@ const processPage = async () => {
 
 export const Scraper = async () => {
     const results = await processPage();
-    console.log('Results:', results);
-    console.log('Results len:', results.length);
+    console.log('number url:', results.length);
     return results;
-    //) }
 };
